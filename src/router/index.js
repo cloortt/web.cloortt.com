d436efbdf2dde077aed9b4107627e5ace2aec603
Onimisi <oniimiisii.a@gmail.com>
2025-08-08 16:01:42 +0100

    Added HomeView Route And 404 (Routes to HomeView)

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,18 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '@/views/HomeView.vue';
+
+const routes =[
+  {
+    path:'/',
+    name:'homepage',
+    component: HomeView
+  },
+  {
+        path:'/:patchMatch(.*)*',
+        name: 'not-found',
+        component: HomeView
+  }
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
