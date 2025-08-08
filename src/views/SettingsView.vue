81cf8572ee7ecdd1f8dcee031a54a6717400e0c5
Onimisi <oniimiisii.a@gmail.com>
2025-08-09 00:07:18 +0100

    Media Query For SettingsView

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -10,45 +10,27 @@
             <AppSettingsGroup>
                 <AppSettingsHeader headerName="Profile" />
                 <div class="content">
-                    <AppSettingsChild childName="Business Information" iconName="users"/>
+                    <AppSettingsChild childName="Business Information" iconName="users" routeName="/settings/business-info"/>
+                    <AppSettingsChild childName="Subscription" iconName="vip-crown" routeName="/settings/subscription"/>
+                </div>
+            </AppSettingsGroup>
+
+            <AppSettingsGroup>
+                <AppSettingsHeader headerName="Preferences" />
+                <div class="content">
+                    <AppSettingsChild childName="Notification" iconName="notification" routeName="/settings/notification"/>
+                    <AppSettingsChild childName="Security" iconName="key" routeName="/settings/security"/>
+                </div>
+            </AppSettingsGroup>
+
+            <AppSettingsGroup>
+                <AppSettingsHeader headerName="Learn more" />
+                <div class="content">
+                    <AppSettingsChild childName="Feedback" iconName="feedback" routeName="/business-information"/>
+                    <AppSettingsChild childName="Legal" iconName="file-info" routeName="/security"/>
                 </div>
             </AppSettingsGroup>
             
-
-
-            <div class="group">
-                <div class="header cl-bg-base">
-                    <h6 class="cl-fg-base-faint">
-                        <small>Profile</small>
-                    </h6>
-                </div>
-                <div class="content">
-                    <div class="child cl-bg-base-faint cl-flex cl-flex-align-items-center">
-                        <div class="icon">
-                            <Icon class="cl-fill-base" name="users" :size="{ width: '20px', height: '20px' }" />
-                        </div>
-                        <div class="text">
-                            <p class="cl-fg-base">Business Information</p>
-                        </div>
-                        <div class="icon cl-flex cl-flex-justify-content-end">
-                            <Icon class="cl-fill-base" name="arrow-right" :size="{ width: '20px', height: '20px' }" />
-                        </div>
-                    </div>
-
-                    <div class="child cl-bg-base-faint cl-flex cl-flex-align-items-center">
-                        <div class="icon">
-                            <Icon class="cl-fill-base" name="users" :size="{ width: '20px', height: '20px' }" />
-                        </div>
-                        <div class="text">
-                            <p class="cl-fg-base">Subscription</p>
-                        </div>
-                        <div class="icon cl-flex cl-flex-justify-content-end">
-                            <Icon class="cl-fill-base" name="arrow-right" :size="{ width: '20px', height: '20px' }" />
-                        </div>
-                    </div>
-                </div>
-            </div>
-
         </div>
     </div>
 </template>
