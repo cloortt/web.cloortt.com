0b82bc0db7b6d51dbe5e84f327524d9e0d2bd0da
Onimisi <oniimiisii.a@gmail.com>
2025-08-08 23:30:43 +0100

    AppSettingsChild Component Created

---
+++ src/components/AppSettingsChild.vue
@@ -0,0 +1,28 @@
+<script setup>
+    import { defineProps } from 'vue';
+    import Icon from './Icon.vue';
+
+    const props = defineProps({
+        childName:{
+            type:String,
+            default:'Child Component'
+        },
+        iconName:{
+            type:String,
+            default:'cloortt'
+        }   
+    });
+</script>
+<template>
+    <div class="child cl-bg-base-faint cl-flex cl-flex-align-items-center">
+        <div class="icon">
+            <Icon class="cl-fill-base" :name="props.iconName" :size="{ width: '20px', height: '20px' }" />
+        </div>
+        <div class="text">
+            <p class="cl-fg-base">{{props.childName}}</p>
+        </div>
+        <div class="icon cl-flex cl-flex-justify-content-end">
+            <Icon class="cl-fill-base" name="arrow-right" :size="{ width: '20px', height: '20px' }" />
+        </div>
+    </div>
+</template>
--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,6 +1,7 @@
 <script setup>
     import Icon from '@/components/Icon.vue';
     import AppSettingsHeader from '@/components/AppSettingsHeader.vue';
+    import AppSettingsChild from '@/components/AppSettingsChild.vue';
 </script>
 <template>
     <div class="web-settings">
@@ -8,29 +9,7 @@
             <div class="group">
                 <AppSettingsHeader headerName="Profile" />
                 <div class="content">
-                    <div class="child cl-bg-base-faint cl-flex cl-flex-align-items-center">
-                        <div class="icon">
-                            <Icon class="cl-fill-base" name="users" :size="{ width: '20px', height: '20px' }" />
-                        </div>
-                        <div class="text">
-                            <p class="cl-fg-base">Business Information</p>
-                        </div>
-                        <div class="icon cl-flex cl-flex-justify-content-end">
-                            <Icon class="cl-fill-base" name="arrow-right" :size="{ width: '20px', height: '20px' }" />
-                        </div>
-                    </div>
-
-                    <div class="child cl-bg-base-faint cl-flex cl-flex-align-items-center">
-                        <div class="icon">
-                            <Icon class="cl-fill-base" name="users" :size="{ width: '20px', height: '20px' }" />
-                        </div>
-                        <div class="text">
-                            <p class="cl-fg-base">Subscription</p>
-                        </div>
-                        <div class="icon cl-flex cl-flex-justify-content-end">
-                            <Icon class="cl-fill-base" name="arrow-right" :size="{ width: '20px', height: '20px' }" />
-                        </div>
-                    </div>
+                    <AppSettingsChild childName="Business Information" iconName="users"/>
                 </div>
             </div>
 
