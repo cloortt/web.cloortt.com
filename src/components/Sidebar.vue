04639cf329aa2e286d1942583f683e7ea3995552
Onimisi <oniimiisii.a@gmail.com>
2025-08-08 14:34:59 +0100

    Created SidebarLink Component

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,6 +1,28 @@
 <script setup>
+    import Icon from './Icon.vue';
+    import SidebarLink from './SidebarLink.vue';
 </script>
 <template>
     <aside class="web-sidebar cl-pos-fixed cl-pos left-offset top-offset cl-size full-height cl-bg-base">
+        <div class="top">
+            <h6 class="cl-fg-base-mild cl-txt w-bold">Cl</h6>
+            <div class="cl-breaker-1"></div>
+            <p class="cl-fg-base-mild">
+                Premium Plan
+            </p>
+            <div class="cl-breaker-1"></div>
+            <div class="cl-badge cl-display-inline cl-bg-base-mild cl-fg-base cl-border edge-is-curved cl-txt w-bold">
+                Ceo
+            </div>
+        </div>
+        <div class="cl-breaker-4"></div>
+        <div class="cl-breaker-4"></div>
+        <div class="links">
+            <ul class="cl-list remove-style">
+                <SidebarLink linkName="Home" linkUrl="/" iconName="tv"/>
+                <SidebarLink linkName="Customers" linkUrl="/customers" iconName="users"/>
+                <SidebarLink linkName="Home" linkUrl="/" iconName="tv"/>
+            </ul>
+        </div>
     </aside>
 </template>
---
+++ src/components/SidebarLink.vue
@@ -0,0 +1,28 @@
+<script setup>
+    import Icon from './Icon.vue';
+    import { defineProps } from 'vue';
+    import { RouterLink } from 'vue-router';
+
+    const props = defineProps({
+        linkName:{
+            type:String,
+            default:'Home'
+        },
+        linkUrl:{
+            type:String,
+            default:'/'
+        },
+        iconName:{
+            type:String,
+            default:'cloortt'
+        }
+    });
+</script>
+<template>
+    <li class="cl-flex-inline cl-flex-align-items-center cl-flex-justify-content-space-between cl-size full-width">
+        <RouterLink :to="props.linkUrl" class="cl-txt no-decoration w-bold cl-fg-base-mild">
+            {{props.linkName}}
+        </RouterLink>
+        <Icon class="cl-fill-base-mild" :name="props.iconName" :size="{ width: '22px', height: '22px' }" />
+    </li>
+</template>
