ee50697c76d471ef5b32421980ff7a8078a4adc0
Onimisi <oniimiisii.a@gmail.com>
2025-08-09 23:59:43 +0100

    Test Changes

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -11,12 +11,12 @@
 <template>
      <div class="the-head cl-flex cl-flex-align-items-center">
         <div class="col-1">
-            <h6 class="cl-fg-black cl-txt no-line-height">{{props.routeName}}</h6>
+            <h6 class="cl-fg-base cl-txt no-line-height">{{props.routeName}}</h6>
         </div>
         <div class="col-2 cl-flex cl-flex-justify-content-end">
             <ul class="cl-list remove-style cl-flex-inline cl-flex-align-items-center">
                 <li class="cl-display-inline-block cl-bg-white cl-flex cl-flex-align-items-center cl-flex-justify-content-center cl-border width-1 style-solid bc-grey edge-is-blunts">
-                    <Icon class="cl-fill-black" name="notification" :size="{ width: '24px', height: '24px' }" />
+                    <Icon class="cl-fill-base" name="notification" :size="{ width: '24px', height: '24px' }" />
                 </li>
 
             </ul>
--- src/components/AppOrders.vue
+++ src/components/AppOrders.vue
@@ -5,52 +5,34 @@
     <div class="child cl-bg-white cl-border width-1 style-solid bc-grey edge-is-blunts">
         <div class="header cl-flex cl-flex-align-items-center cl-flex-justify-content-space-between">
             <div class="status">
-                <div class="cl-badge cl-display-inline cl-bg-base-faint cl-fg-base cl-border edge-is-curved cl-txt w-bold">
+                <div class="cl-badge cl-display-inline cl-bg-base-faint cl-fg-base cl-txt w-bold">
                     In Progress
                 </div>
             </div>
             <div class="action cl-flex cl-flex-justify-content-end">
-                <Icon class="cl-fill-black" name="file-edit" :size="{ width: '20px', height: '20px' }" />
+                <Icon class="cl-fill-base" name="file-edit" :size="{ width: '20px', height: '20px' }" />
             </div>
         </div>
         <div class="info">
             <div class="group cl-flex cl-flex-align-items-center">
                 <div class="title">
-                    <p class="cl-fg-black cl-txt w-bold">Customer</p>
+                    <p class="cl-fg-base cl-txt w-bold">Customer</p>
                 </div>
                 <div class="description cl-flex cl-flex-justify-content-end">
-                    <p class="cl-fg-black">Omas-e</p>
+                    <p class="cl-fg-base">Omas-e</p>
                 </div>
             </div>
 
             <div class="group cl-flex cl-flex-align-items-center">
                 <div class="title">
-                    <p class="cl-fg-black cl-txt w-bold">Style</p>
+                    <p class="cl-fg-base cl-txt w-bold">Timeline</p>
                 </div>
                 <div class="description cl-flex cl-flex-justify-content-end">
-                    <p class="cl-fg-black">Ankara Gown</p>
+                    <p class="cl-fg-base">09 - 08 - 25</p>
                 </div>
             </div>
 
-            <div class="group cl-flex cl-flex-align-items-center">
-                <div class="title">
-                    <p class="cl-fg-black cl-txt w-bold">Timeline</p>
-                </div>
-                <div class="description cl-flex cl-flex-justify-content-end">
-                    <p class="cl-fg-black">09 - 08 - 25</p>
-                </div>
-            </div>
-
-            <div class="group cl-flex cl-flex-align-items-center">
-                <div class="title">
-                    <p class="cl-fg-black cl-txt w-bold">Price</p>
-                </div>
-                <div class="description cl-flex cl-flex-justify-content-end">
-                    <p class="cl-fg-black">7,000</p>
-                </div>
-            </div>
-
-            <div class="group cl-flex cl-flex-align-items-center">
+            <!-- <div class="group cl-flex cl-flex-align-items-center">
                 <div class="title">
                     <p class="cl-fg-black cl-txt w-bold">Payment Status</p>
                 </div>
@@ -58,6 +40,24 @@
                     <div class="cl-badge cl-display-inline cl-bg-base-faint cl-fg-base cl-border edge-is-curved cl-txt w-bold">
                         Pending
                     </div>
+                </div>
+            </div> -->
+
+            <div class="group cl-flex cl-flex-align-items-center">
+                <div class="title">
+                    <p class="cl-fg-base cl-txt w-bold">Design</p>
+                </div>
+                <div class="description cl-flex cl-flex-justify-content-end">
+                    <p class="cl-fg-base">Ankara Gown</p>
+                </div>
+            </div>
+
+            <div class="group cl-flex cl-flex-align-items-center">
+                <div class="title">
+                    <p class="cl-fg-base cl-txt w-bold">Price</p>
+                </div>
+                <div class="description cl-flex cl-flex-justify-content-end">
+                    <p class="cl-fg-base">7,000</p>
                 </div>
             </div>
             
