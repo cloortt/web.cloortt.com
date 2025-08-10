5e6e44b2a980965a8cd62c155f494cc14a1c6513
Onimisi <oniimiisii.a@gmail.com>
2025-08-10 01:07:20 +0100

    Modified web.css, Modified AppOrder Component, Modified OrdersView

--- src/components/AppOrders.vue
+++ src/components/AppOrders.vue
@@ -2,7 +2,7 @@
     import Icon from './Icon.vue';
 </script>
 <template>
-    <div class="child cl-bg-white cl-border width-1 style-solid bc-grey">
+    <div class="child">
         <div class="header cl-flex cl-flex-align-items-center cl-flex-justify-content-space-between">
             <div class="status">
                 <div class="cl-badge cl-display-inline cl-bg-base cl-fg-base-mild cl-txt w-bold">
--- src/views/OrdersView.vue
+++ src/views/OrdersView.vue
@@ -2,7 +2,7 @@
     import AppOrder from '@/components/AppOrders.vue';
 </script>
 <template>
-    <div class="web-orders cl-grid">
+    <div class="web-orders">
         <AppOrder/>
         <AppOrder/>
         <AppOrder/>
