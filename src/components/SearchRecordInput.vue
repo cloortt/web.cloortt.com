14e796b0227a95188ee70601e6cbdbe4095c934c
Onimisi <oniimiisii.a@gmail.com>
2025-08-11 14:47:43 +0100

    Modified SearchRecordInput Component (Changed Input Border Radius), Modified cloortt.css (Changed left and right padding input space for lg form)

--- src/components/SearchRecordInput.vue
+++ src/components/SearchRecordInput.vue
@@ -13,10 +13,11 @@
  });
 </script>
 <template>
+    <div class="cl-breaker-1"></div>
      <form class="cl-form">
         <div>
-            <input type="text" class="lg cl-border width-1 style-solid bc-grey edge-is-curved cl-size full-width" v-model="props.searchValue" :placeholder="props.placeholder" >
+            <input type="text" class="lg cl-bg-white cl-border width-1 style-solid bc-grey edge-is-blunt cl-size full-width" v-model="props.searchValue" :placeholder="props.placeholder" >
         </div>
     </form>
-    <div class="cl-breaker-2"></div>
+    <div class="cl-breaker-1"></div>
 </template>
