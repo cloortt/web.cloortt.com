41a63e2521fd249ebfe82ac70dfb6a672cf2354d
Onimisi <oniimiisii.a@gmail.com>
2025-08-14 00:13:24 +0100

    Create AppAnalyticsCard Component, Modified AnalyticsView, Modified Cloortt.css

--- src/views/AnalyticsView.vue
+++ src/views/AnalyticsView.vue
@@ -1,5 +1,17 @@
 <script setup>
+    import AppAnalyticsCard from '@/components/AppAnalyticsCard.vue';
+    import { ref } from 'vue';
+
+    const customersFilter = ref(['All','Pending']);
+    const ordersFilter = ref(['All','Pending']);
+    const pricingFilter = ref(['All','Pending']);
+    const catalogFilter = ref(['All','Pending']);
 </script>
 <template>
-    
+    <div class="web-analytics cl-grid grid-4 stackable">
+        <AppAnalyticsCard cardName="Customers" cardIcon="users" cardValue="10" :cardFilter="customersFilter"/>
+        <AppAnalyticsCard cardName="Orders" cardIcon="bag" cardValue="70" :cardFilter="ordersFilter"/>
+        <AppAnalyticsCard cardName="Pricing" cardIcon="bank-card" cardValue="5" :cardFilter="pricingFilter"/>
+        <AppAnalyticsCard cardName="Catalog" cardIcon="image-ai" cardValue="11" :cardFilter="catalogFilter"/>
+    </div>
 </template>
