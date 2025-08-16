d40995fba7f84ed3cb1fcfb37fa34865df02d9f1
Onimisi <oniimiisii.a@gmail.com>
2025-08-16 01:13:41 +0100

    Deleted All AppSettings Components

--- src/components/AppSettingsGroup.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="group cl-border width-1 style-solid bc-grey">
-        <slot></slot>
-    </div>
-</template>
--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,36 +1,8 @@
 <script setup>
     import Icon from '@/components/Icon.vue';
-    import AppSettingsGroup from '@/components/AppSettingsGroup.vue';
-    import AppSettingsHeader from '@/components/AppSettingsHeader.vue';
-    import AppSettingsChild from '@/components/AppSettingsChild.vue';
 </script>
 <template>
-    <div class="web-settings">
-        <div class="frame">
-            <AppSettingsGroup>
-                <AppSettingsHeader headerName="Profile" />
-                <div class="content">
-                    <AppSettingsChild childName="Business Information" iconName="users" routeName="/settings/business-info"/>
-                    <AppSettingsChild childName="Subscription" iconName="vip-crown" routeName="/settings/subscription"/>
-                </div>
-            </AppSettingsGroup>
-
-            <AppSettingsGroup>
-                <AppSettingsHeader headerName="Preferences" />
-                <div class="content">
-                    <AppSettingsChild childName="Notification" iconName="notification" routeName="/settings/notification"/>
-                    <AppSettingsChild childName="Security" iconName="key" routeName="/settings/security"/>
-                </div>
-            </AppSettingsGroup>
-
-            <AppSettingsGroup>
-                <AppSettingsHeader headerName="Learn more" />
-                <div class="content">
-                    <AppSettingsChild childName="Feedback" iconName="feedback" routeName="/business-information"/>
-                    <AppSettingsChild childName="Legal" iconName="file-info" routeName="/security"/>
-                </div>
-            </AppSettingsGroup>
-            
-        </div>
+    <div class="web-settings cl-grid grid-3 stackable">
+       
     </div>
 </template>
