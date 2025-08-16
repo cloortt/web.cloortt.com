e177bc3818b68eff8121fa6716caf1548d7d5005
Onimisi <oniimiisii.a@gmail.com>
2025-08-16 02:32:38 +0100

    Created AppSettings Component, Modified SettingsView, Modified SidebarLayout Component, Modified web.css, Modified cloortt.css (Changed Font)

---
+++ src/components/AppSettings.vue
@@ -0,0 +1,38 @@
+<script setup>
+    import Icon from '@/components/Icon.vue';
+    import { defineProps } from 'vue';
+
+    const props = defineProps({
+        title:{
+            type:String,
+            default:''
+        },
+        description:{
+            type:String,
+            default:''  
+        },
+        icon:{
+            type:String,
+            default:'cloortt'
+        }
+    });
+</script>
+<template>
+    <div class="child cl-inner-space-3">
+        <div class="top">
+            <div class="cl-flex cl-flex-align-items-center cl-flex-justify-content-space-between">
+                <div>
+                    <h6 class="cl-fg-base cl-txt w-bold">
+                        <small>{{props.title}}</small>
+                    </h6>
+                </div>
+                <div>
+                     <Icon class="cl-fill-base" :name="props.icon" :size="{ width: '25px', height: '25px' }" />
+                </div>
+            </div>
+        </div>
+        <div class="bottom">
+            <p class="cl-fg-base">{{ props.description }}</p>
+        </div>
+    </div>
+</template>
--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,8 +1,43 @@
 <script setup>
-    import Icon from '@/components/Icon.vue';
+    import AppSettings from '@/components/AppSettings.vue';
 </script>
 <template>
-    <div class="web-settings cl-grid grid-3 stackable">
-       
+    <div class="web-settings">
+       <AppSettings 
+            icon="users" 
+            title="Profile" 
+            description="Preview and manage your business information"
+        />
+
+        <AppSettings 
+            icon="vip-crown" 
+            title="Subscription" 
+            description="Control and manage your subscription plan"
+        />
+
+        <AppSettings 
+            icon="notification" 
+            title="Notification" 
+            description="Control and manage your notifications"
+        />
+
+        <AppSettings 
+            icon="key" 
+            title="Security" 
+            description="Control and manage your security"
+        />
+
+        <AppSettings 
+            icon="feedback" 
+            title="Contact" 
+            description="Report a bug, or request a feature"
+        />
+
+        <AppSettings 
+            icon="file-info" 
+            title="About" 
+            description="Privacy policy, terms of service, and more"
+        />
+        
     </div>
 </template>
