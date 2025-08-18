9d26d201e61a1ff9f7086750e6c21bca887eaf09
Onimisi <oniimiisii.a@gmail.com>
2025-08-19 00:57:17 +0100

    Modified Icon Component

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -392,6 +392,34 @@ const props = defineProps({
   </svg>
   <!-- File History Ends -->
 
+  <!-- Equal Icon Starts -->
+  <svg
+    v-else-if="props.name === 'equal'"
+    :width="props.size.width"
+    :height="props.size.height"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+  >
+    <path d="M19 8H5V10H19V8ZM19 14H5V16H19V14Z"></path>
+  </svg>
+  <!-- Equal Icon Ends -->
+
+  <!-- Close Icon Starts  -->
+  <svg
+    v-else-if="props.name === 'close'"
+    :width="props.size.width"
+    :height="props.size.height"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+  >
+    <path
+      d="M11.9997 10.5865L16.9495 5.63672L18.3637 7.05093L13.4139 12.0007L18.3637 16.9504L16.9495 18.3646L11.9997 13.4149L7.04996 18.3646L5.63574 16.9504L10.5855 12.0007L5.63574 7.05093L7.04996 5.63672L11.9997 10.5865Z"
+    ></path>
+  </svg>
+  <!-- Close Icon Ends -->
+
   <!-- Alternative Icon Starts -->
   <svg
     v-else
