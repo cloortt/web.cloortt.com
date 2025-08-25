72b2585e2fd2bfe6a4ebcb5b40ec4c82756de77e
Onimisi <oniimiisii.a@gmail.com>
2025-08-25 23:56:29 +0100

    Modified Cloortt.css, Created Modal Component, Modified PricingView

---
+++ src/components/Modal.vue
@@ -0,0 +1,71 @@
+<script setup>
+import Icon from './Icon.vue'
+import { defineProps, defineEmits, watch } from 'vue'
+
+const props = defineProps({
+  title: {
+    type: String,
+    default: 'C',
+  },
+  type: {
+    type: String,
+    default: 'small',
+  },
+  visibility: {
+    type: Boolean,
+    default: false,
+  },
+})
+
+const emits = defineEmits(['close'])
+// Watch for changes in the isVisible prop
+watch(
+  () => props.visibility,
+  (newVal) => {
+    if (newVal) {
+      document.body.style.overflow = 'hidden'
+    } else {
+      document.body.style.overflow = 'auto'
+    }
+  },
+)
+
+const closeModal = () => {
+  emits('close')
+}
+</script>
+<template>
+  <section
+    v-if="props.visibility"
+    @click.self="closeModal"
+    class="cl-modal cl-pos-fixed left-offset top-offset cl-size full-width full-height cl-overlay-dark cl-flex-align-items-center cl-flex-justify-content-center"
+  >
+    <div
+      class="content-box cl-bg-white"
+      :class="[
+        `sz-${props.type}`,
+        {
+          'cl-border style-solid width-1 bc-base':
+            props.type === 'small' || props.type === 'medium' || props.type === 'large',
+        },
+      ]"
+    >
+      <div class="the-head cl-flex cl-flex-align-items-center">
+        <div class="the-title">
+          <h6 class="cl-fg-base">{{ props.title }}</h6>
+        </div>
+        <div class="icon cl-flex cl-flex-justify-content-end">
+          <Icon
+            @click="closeModal"
+            class="cl-fill-base"
+            name="close"
+            :size="{ width: '20px', height: '20px' }"
+          />
+        </div>
+      </div>
+      <div class="the-body cl-inner-space-3">
+        <slot></slot>
+      </div>
+    </div>
+  </section>
+</template>
--- src/views/PricingView.vue
+++ src/views/PricingView.vue
@@ -1,8 +1,13 @@
 <script setup>
+import { ref } from 'vue'
 import SearchRecordInput from '@/components/SearchRecordInput.vue'
 import AppPricing from '@/components/AppPricing.vue'
+import Modal from '@/components/Modal.vue'
+
+const openNewPriceModal = ref(false)
 </script>
 <template>
+  <button @click="openNewPriceModal = true">Open</button>
   <SearchRecordInput searchValue="" placeholder="Search Pricing..." />
   <div class="web-pricing">
     <AppPricing
@@ -56,4 +61,52 @@ import AppPricing from '@/components/AppPricing.vue'
       image="https://plus.unsplash.com/premium_photo-1671221672467-e87b65cb07df?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTN8fHRyb3VzZXJ8ZW58MHx8MHx8fDA%3D"
     />
   </div>
+
+  <Modal
+    title="Pricing"
+    type="sidebar"
+    :visibility="openNewPriceModal"
+    @close="openNewPriceModal = false"
+  >
+    <form class="cl-form">
+      <div>
+        <img
+          class="cl-image cl-display-block tiny-size"
+          src="https://plus.unsplash.com/premium_photo-1671221672467-e87b65cb07df?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTN8fHRyb3VzZXJ8ZW58MHx8MHx8fDA%3D"
+          alt="Picture"
+        />
+      </div>
+      <div class="cl-breaker-4"></div>
+      <div class="cl-breaker-4"></div>
+      <div>
+        <p class="cl-fg-base cl-txt w-bold">Name <span class="cl-fg-danger">*</span></p>
+        <div class="cl-breaker-2"></div>
+        <input
+          class="lg cl-bg-white cl-border width-1 style-solid bc-grey cl-size full-width"
+          type=""
+          placeholder="E.g Pants"
+        />
+      </div>
+      <div class="cl-breaker-4"></div>
+      <div class="cl-breaker-4"></div>
+      <div>
+        <p class="cl-fg-base cl-txt w-bold">Price <span class="cl-fg-danger">*</span></p>
+        <div class="cl-breaker-2"></div>
+        <input
+          class="lg cl-bg-white cl-border width-1 style-solid bc-grey cl-size full-width"
+          type=""
+          placeholder="10,000"
+        />
+      </div>
+      <div class="cl-breaker-4"></div>
+      <div class="cl-breaker-4"></div>
+      <div class="cl-flex cl-flex-justify-content-end">
+        <button
+          class="cl-button lg cl-txt w-bold cl-bg-base cl-fg-base-faint cl-border width-1 style-solid bc-base"
+        >
+          Save
+        </button>
+      </div>
+    </form>
+  </Modal>
 </template>
